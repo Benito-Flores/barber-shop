bbd814fc351b9ec69ccc8e16afe46e0b7c2d9ff9
Benito Flores <118690844+Benito-Flores@users.noreply.github.com>
2024-01-02 15:21:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// mobile navigation
+const openNav = document.querySelector('.open-nav');
+const closeNav = document.querySelector('.close-nav');
+const nav = document.querySelector('.nav-container');
+
+openNav.addEventListener('click', ()=> {
+  nav.classList.add('open-navigation');
+});
+
+closeNav.addEventListener('click', ()=> {
+  nav.classList.remove('open-navigation');
+});
