8fdaefe82a0e3ada49bb9affe40779020c32262c
Benito Flores <118690844+Benito-Flores@users.noreply.github.com>
2024-01-02 17:50:08 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ openNav.addEventListener('click', ()=> {
 closeNav.addEventListener('click', ()=> {
   nav.classList.remove('open-navigation');
 });
+
+// adding and removing hover/tab over color to links
+
+const notCurrentPages = document.querySelectorAll('.nav-link');
+const currentPage = document.querySelector('.current-link');
+
+notCurrentPages.forEach(link => {
+  link.addEventListener('mouseover', ()=> {
+    currentPage.classList.remove('current-link');
+  });
+
+  link.addEventListener('mouseout', ()=> {
+    currentPage.classList.add('current-link');
+  });
+
+  link.addEventListener('focus', ()=> {
+    currentPage.classList.remove('current-link');
+  });
+
+  link.addEventListener('blur', ()=> {
+    currentPage.classList.remove('current-link');
+  });
+});
